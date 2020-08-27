588fcb3e0cbbed0efc789305f750971477b24d11
Jenifer Angelo Medeiros <jenmangelo@gmail.com>
2020-08-26 23:53:02 -0300

    Organizando melhor as pastas

---
+++ C/Faculdade/1FAC - Fundamentos de Algoritmos Computacionais/lista1/Lista 1 - Questao 1.c
@@ -0,0 +1,28 @@
+/* Lista 1 | QUESTÃO 01: O custo ao consumidor de um carro novo é a soma do custo de fábrica com
+a porcentagem do distribuidor e dos impostos (aplicados ao custo de fábrica).
+Desenvolver um algoritmo que calcule o custo ao consumidor de determinado carro.   */
+
+#include<stdio.h>
+
+int main ()
+{
+	//Declaração de variáveis
+	float custo, distr, imposto, preco_final;
+
+	//Obter dados
+	printf("Digite o custo do carro\n");
+	scanf("%f", &custo);
+
+	printf("Digite a porcentagem do distribuidor\n");
+	scanf("%f", &distr);
+
+	printf("Digite a porcentagem do imposto\n");
+	scanf("%f", &imposto);
+
+	//Calculando final
+	preco_final = custo*(1+(distr+imposto)/100);
+
+	//Exibir resultados
+	printf("O preco final e %.2f\n", preco_final);
+
+}
